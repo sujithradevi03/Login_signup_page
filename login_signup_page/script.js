543588de7b3acaeb92056a8133cfecdf7f47fdf9
sujithra devi <123398531+sujithradevi03@users.noreply.github.com>
2024-01-23 23:13:26 +0530

    Add files via upload

---
+++ login_signup_page/script.js
@@ -0,0 +1,15 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Add login logic here
+    alert("Login logic goes here");
+  });
+
+document
+  .getElementById("signupForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Add signup logic here
+    alert("Account Created Succesfully!!!");
+  });
